43af009a33b8fca53eeeef76c40427c955df435e
Roberto Barros <roberto.f.barros@gmail.com>
2024-10-19 18:02:32 -0300

    livecode

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+Garden.destroy_all
+Chore.destroy_all
+
+little = Garden.create!(name: 'My First Garden', image_url: "https://images.unsplash.com/photo-1460533893735-45cea2212645?q=80&w=3028&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+other =Garden.create!(name: 'My Little Garden', image_url: "https://images.unsplash.com/photo-1550948390-6eb7fa773072?q=80&w=2948&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+
+Plant.create!(
+  name: "Monstera",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/monstera.jpg",
+  garden: little
+)
+
+Plant.create!(
+  name: "Philo",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/philo.jpg",
+  garden: little
+)
+
+Plant.create!(
+  name: "Dieff",
+  image_url: "https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/plants/dieffenbachia.jpg",
+  garden: other
+)
+
+names = %w(Water Mist Clean Feed Repot Rotate)
+
+names.each do |name|
+  Chore.create!(name: name)
+end
